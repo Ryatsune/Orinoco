8127e26833d7e35ceaee5ce224a205e3725dd9cb
ryatsune <carpentieri.yann@gmail.com>
2020-12-21 13:56:44 +0100

    Transition empty cart / cart & clean code

--- front-end/JS/cart.js
+++ front-end/JS/cart.js
@@ -12,24 +12,38 @@ let teddiePrice = document.querySelector('#teddie-price');
 // console.log(teddiePrice);
 let trashBtn = document.querySelector('#trash-btn');
 // console.log(trashBtn);
+let emptySection = document.querySelector('#empty-cart');
+// console.log(emptySection);
+let paySection = document.querySelector('#pay-form');
+// console.log(paySection);
+
+
+
+if (localStorage.getItem('cart')
+== undefined) {
+    console.log("❌ Panier vide ");
+    
+} else {
+    console.log("✅ Voici votre panier");
+
+    emptySection.classList.remove("d-block");
+    emptySection.classList.add("d-none");
+
+    paySection.classList.remove("d-none");
+    paySection.classList.add("d-block");
+
+}
 
 
 
 //*********** Récupération des données du localStorage ***********//
-// const finalCart = [];
-// console.log(finalCart);
-
-// const addToCart = finalCart.push(JSON.parse(localStorage.getItem("cart")));
-// console.log(addToCart);
-
 //récupération de la key & value du localStorage --> stockées dans finalCart
 let finalCart = JSON.parse(localStorage.getItem('cart'));
-console.log(finalCart);
 
-//boucle forEach pour répeter l'action pour chaque produits
-finalCart.forEach((info) => {
-    
+
 //********** Ajoute à la page les données récupérées de localStorage ***********//
+//boucle forEach pour répeter l'action pour chaque produits
+finalCart.forEach((info) => { 
     teddieImage.src = info.imageUrl;
     teddieName.innerHTML = `Lot d'un ourson ${info.name}`;
     teddiePrice.innerHTML = `${info.price} €`
@@ -46,13 +60,3 @@ finalCart.forEach((info) => {
 //      suppriLocalStorage()
      
 // }
-
-
-
-//article panier = 0
-//localStorage vide
-//Ajout de 1 Norbert au panier
-//localStorage = tableau des objets = Norbert  // [{name : imageUrl : Price : Descirption : ID: }] =Norbert
-//récupérer la value  du localStorage
-//ajout de 1 objet (carl)
-//localStorage = tableau des objets = Norbert  // [{name NORBERT : imageUrl : Price : Descirption : ID:},  {name : CARL imageUrl : Price : Descirption : ID: }]
