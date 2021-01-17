a5c44ec3bf156a21118a6d93953ebf08b1948c92
ryatsune <carpentieri.yann@gmail.com>
2021-01-17 13:57:22 +0100

    initial commit : teddie constructor

---
+++ front-end/JS/teddie.js
@@ -0,0 +1,10 @@
+class Teddie {
+  constructor(teddie) {
+    this._id = teddie._id;
+    this.name = teddie.name;
+    this.imageUrl = teddie.imageUrl;
+    this.description = teddie.description;
+    this.price = teddie.price;
+    this.colors = teddie.colors;
+  }
+}
