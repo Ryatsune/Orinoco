bff4e522b51716d0dd69c0dd74c1f16b900674b7
ryatsune <carpentieri.yann@gmail.com>
2021-01-17 14:26:31 +0100

    remove key 'cart' from localStorage

--- front-end/JS/confirmation.js
+++ front-end/JS/confirmation.js
@@ -1,17 +1,2 @@
-//******* Récupération des données localStorage *******//
-let name = localStorage.getItem('name');
-// console.log(name);
-let price = localStorage.getItem('price');
-// console.log(price);
-
-
-//******* Récupération des span *******//
-let confirmationName = document.querySelector('#teddie-confimation');
-let confirmationPrice = document.querySelector('#price-confirmation');
-
-
-
-//******* Modification des span via données localStorage *******//
-confirmationName.innerHTML = `${name}`;
-confirmationPrice.innerHTML = `${price} €`
-
+//supprime la key cart du localStorage
+localStorage.removeItem('cart');
