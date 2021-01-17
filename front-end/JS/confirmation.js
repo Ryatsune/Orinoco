dacf847d626ba2711d4ed88d2858ffe4457a83b3
ryatsune <carpentieri.yann@gmail.com>
2021-01-17 14:28:33 +0100

    function displayPriceFromStorage

--- front-end/JS/confirmation.js
+++ front-end/JS/confirmation.js
@@ -1,2 +1,11 @@
 //supprime la key cart du localStorage
 localStorage.removeItem('cart');
+
+//récupère le price du localStorage et la span 
+//puis fill la span avec le price
+function displayPriceFromStorage(params) {
+    let finalPrice = JSON.parse(localStorage.getItem('finalPrice'));
+    let spanPrice = document.querySelector('#price-confirmation');
+    spanPrice.innerHTML = finalPrice + '€';
+}
+displayPriceFromStorage();
